4cf30b64c2d089abb224b440cf46d06c04a1c1b4
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-14 16:30:24 +0300

    Make my first random and adding tothe view te result

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,21 +13,19 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
 
-
         rollButton.setOnClickListener { rollDice() }
 
     }
 
     private fun rollDice() {
+        val randomInt = (1..6).random()
 
-//        Toast.makeText(
-//            this, "button clicked",
-//            Toast.LENGTH_SHORT
-//        ).show()
+        Toast.makeText(
+            this, "button clicked",
+            Toast.LENGTH_SHORT
+        ).show()
 
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
-
-
+        resultText.text = randomInt.toString()
     }
 }
